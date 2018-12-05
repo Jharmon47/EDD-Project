da8d1001c4e9995dce11c0611aa9ce98f2e021bc
Yamka, Heather A <19002911@d209macbook027.local>
2018-12-05 11:30:11 -0500

    ccccccc

--- Tester/ViewController.swift
+++ Tester/ViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var login: UIButton!
-    @IBOutlet weak var signup: UIButton!
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    @IBAction func login(sender: AnyObject) {
+        //
+        if(username.text == "test" && password.text == "test"){
+            //go to next page
+            username.text = "IT WORKED";
+        }
+        //give error
+    }
+    
+    
     override func viewDidLoad() {
         //test2
         super.viewDidLoad()
@@ -22,7 +34,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+
+}
+class SignupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        //test2
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
